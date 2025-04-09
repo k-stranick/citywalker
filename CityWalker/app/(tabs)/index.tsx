cdc9c32fffe41c2ba6522ad3ecf47e2a50b1250c
Kyle <kstranic@dtcc.edu>
2025-04-09 12:47:36 -0400

    skeleton for the project

--- CityWalker/app/(tabs)/index.tsx
+++ CityWalker/app/(tabs)/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, Button, SafeAreaView } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <Text>City Walker</Text>
+      <Link href="/(tabs)/weather">Check Ski Conditions</Link>
+      <Link href="/(tabs)/restaurants">Explore the Fine Cuisine</Link>
+      <Link href="/(tabs)/hikingTrails">Hike the Trails</Link>
+    </SafeAreaView>
   );
 }
